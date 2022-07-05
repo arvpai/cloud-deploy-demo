1f9f5628363cbd076f32644f193092941990231b
Matt Gilliam <50531124+MaGilli81@users.noreply.github.com>
2022-07-05 17:39:51 -0500

    Update setupscript.sh

--- setupscript.sh
+++ setupscript.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-##### !!!!!!! CHANGE GITHUB USERNAME AND EMAIL ADDRESS BELOW !!!!!! ######
-
 #### SET PROJECT VARIABLES ####
+export GITHUB_USERNAME=
+export GITHUB_USEREMAIL=
 export PROJECT_ID=$(gcloud config get-value core/project)
 export PROJECT_NUMBER="$(gcloud projects describe ${PROJECT_ID} --format='get(projectNumber)')"
+export AR_REPOSITORY=my-repository
+export GKE_CLUSTER=hello-cloudbuild
+export REGION=us-central1
+export CSR_REPOSITORY_APP=hello-cloudbuild-app
+export CSR_REPOSITORY_ENV=hello-cloudbuild-env
+export TRIGGER_CI=hello-cloudbuild-ci
+export TRIGGER_DEPLOY=hello-cloudbuild-deploy
+
 gcloud config set project $PROJECT_ID
 
 #### ENABLE APIS ####
@@ -23,21 +31,21 @@ gcloud services enable container.googleapis.com \
     iam.googleapis.com
 
 #### CREATE A GKE STANDARD CLUSTER ####
-gcloud container clusters create hello-cloudbuild \
-    --num-nodes 1 --region us-central1
+gcloud container clusters create ${GKE_CLUSTER} \
+    --num-nodes 1 --region ${REGION}
 
 #### CREATE AN ARTIFACT REPOSITORY ####
-gcloud artifacts repositories create my-repository \
+gcloud artifacts repositories create ${AR_REPOSITORY} \
   --repository-format=docker \
-  --location=us-central1
+  --location=${REGION}
 
 #### IDENTIFY AUTHOR OF COMMITS
-git config --global user.email "GITHUB USER EMAIL ADDRESS"
-git config --global user.name "GITHUB USER NAME"
+git config --global user.email "${GITHUB_USEREMAIL}"
+git config --global user.name "${GITHUB_USERNAME}"
 
 # CREATE A REPO IN CLOUD SOURCE REPOSITORY
-gcloud source repos create hello-cloudbuild-app
-gcloud source repos create hello-cloudbuild-env
+gcloud source repos create ${CSR_REPOSITORY_APP}
+gcloud source repos create ${CSR_REPOSITORY_ENV}
 
 # #### Add gke hub admin rolel to user in IAM #####
 # #### REGISTER THE CLUSTER TO ANTHOS ####
@@ -47,14 +55,13 @@ gcloud source repos create hello-cloudbuild-env
 
 #### CLONE A GITHUB REPOSITORY FOR THE HELLO-CLOUDBUILD-APP SOURCE CODE #### 
 cd ~
-git clone https://github.com/MaGilli81/gke-gitops-tutorial \
+git clone https://github.com/MaGilli81/gke-gitops-tutorial-cloudbuild \
     hello-cloudbuild-app
 
 #### CONFIGURE HELLO-CLOUDBUILD-APP CSR REPO AS THE REMOTE #### 
 cd ~/hello-cloudbuild-app
 git remote add google \
     "https://source.developers.google.com/p/${PROJECT_ID}/r/hello-cloudbuild-app"
-
 
 #### TAG THE HELLO-CLOUDBUILD-APP WITH THE LATEST COMMIT_SHA(THIS IS PICKED UP FROM CLOUD BUILD)####
 COMMIT_ID="$(git rev-parse --short=7 HEAD)"
@@ -63,13 +70,14 @@ gcloud builds submit --tag="us-central1-docker.pkg.dev/${PROJECT_ID}/my-reposito
 #### CREATE CLOUD BUILD TRIGGER FOR CONTINUOUS  INTEGRATION PIPELINE FOR HELLO-CLOUDBUILD-APP
 #### CLOUD BUILD CI TRIGGER SHOULD CONTAIN A CLOUD BUILD YAML FILE THAT PACKAGES, CONTAINERIZES AND TAGS AN IMAGE BEFORE PUSHING IMAGE TO ARTIFACT REGISTRY ####
 gcloud beta builds triggers create cloud-source-repositories \
-    --repo=hello-cloudbuild-app \
+    --repo=${CSR_REPOSITORY_APP} \
     --branch-pattern=^master$ \
     --build-config=/cloudbuild.yaml \
-    --name=hello-cloudbuild-ci
+    --name=${TRIGGER_CI}
 
-git config --global user.email "GITHUB USER EMAIL ADDRESS"
-git config --global user.name "GITHUB USER NAME"
+#### IDENTIFY AUTHOR OF COMMITS
+git config --global user.email "${GITHUB_USEREMAIL}"
+git config --global user.name "${GITHUB_USERNAME}"
 
 #### PUSH APPLICATION CODE TO THE HELLO-CLOUDBUILD-APP CSR REPO'S MASTER BRANCH TO START THE CI PROCESS
 cd ~/hello-cloudbuild-app
@@ -95,7 +103,7 @@ cd ~/hello-cloudbuild-env
 cp ~/hello-cloudbuild-app/cloudbuild-delivery.yaml ~/hello-cloudbuild-env/cloudbuild.yaml
 
 git add .
-git commit -m "Create cloudbuild.yaml for deployment and cp kubernetes yaml"
+git commit -m "Create cloudbuild.yaml for deployment"
 
 #### CREATE A CANDIDATE BRANCH AND PUSH TO UPDATE THE PRODUCTION AND CANDIDATE BRANCH ####
 git push origin production
@@ -115,10 +123,10 @@ gcloud source repos set-iam-policy \
 #### CREATE CLOUD BUILD TRIGGER FOR CONTINUOS DELIVER PIPELINE FOR HELLO-CLOUDBUILD-APP
 #### CLOUD BUILD CD TRIGGER SHOULD CONTAIN A CLOUD BUILD YAML FILE THAT GRABS THE IMAGE FROM ARTIFACT REGISTRY AND DEPLOYS IT TO GKE, OR CLOUD DEPLOY ####
 gcloud beta builds triggers create cloud-source-repositories \
-    --repo=hello-cloudbuild-env \
+    --repo=${CSR_REPOSITORY_ENV} \
     --branch-pattern=^candidate$ \
     --build-config=/cloudbuild.yaml \
-    --name=hello-cloudbuild-deploy
+    --name=${TRIGGER_DEPLOY}
 
 #### KICK OFF THE CI BY GENERATING A NEW VERSION BY REPLACING THE CLOUDBUILD.YAML FILE WITH THE CLOUDBUILD-TRIGGER-CD.YAML FILE ####
 cd ~/hello-cloudbuild-app
@@ -139,8 +147,8 @@ git push google master
 
 #### REGISTER CLOUD DEPLOY DELIVERY PIPELINE ####
 cd ..
-gcloud deploy apply --file=delivery-pipeline.yaml --region=us-central1 && \
-gcloud deploy apply --file=target_dev.yaml --region=us-central1
+gcloud deploy apply --file=delivery-pipeline.yaml --region=${REGION} && \
+gcloud deploy apply --file=target_dev.yaml --region=${REGION}
 
 #### CREATE A RELEASE FOR THE CLOUD DEPLOY DELIVERY PIPELINE ####
-gcloud deploy releases create my-release --delivery-pipeline=hello-cloudbuild-delivery-pipeline --region=us-central1
+gcloud deploy releases create my-release --delivery-pipeline=hello-cloudbuild-delivery-pipeline --region=${REGION}
