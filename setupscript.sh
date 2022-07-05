5b6c8a101ab74d6dbd653adef1f388e1b352de48
Matt Gilliam <50531124+MaGilli81@users.noreply.github.com>
2022-07-05 17:15:30 -0500

    Update setupscript.sh

--- setupscript.sh
+++ setupscript.sh
@@ -68,6 +68,9 @@ gcloud beta builds triggers create cloud-source-repositories \
     --build-config=/cloudbuild.yaml \
     --name=hello-cloudbuild-ci
 
+git config --global user.email "GITHUB USER EMAIL ADDRESS"
+git config --global user.name "GITHUB USER NAME"
+
 #### PUSH APPLICATION CODE TO THE HELLO-CLOUDBUILD-APP CSR REPO'S MASTER BRANCH TO START THE CI PROCESS
 cd ~/hello-cloudbuild-app
 git push google master
@@ -79,25 +82,24 @@ gcloud projects add-iam-policy-binding ${PROJECT_NUMBER} \
     --role=roles/container.developer \
     --role=roles/source.writer \
     --role=roles/clouddeploy.admin \
-    --role=roles/clouddeploy.jobRunner \
-    --role=roles/container.developer
+    --role=roles/clouddeploy.jobRunner
 
 #### CLONE THE HELLO-CLOUDBUILD-ENV REPO AND CREATE A PRODUCTION BRANCH
 cd ~
 gcloud source repos clone hello-cloudbuild-env
 cd ~/hello-cloudbuild-env
 git checkout -b production
-git checkout -b candidate
 
 #### Copy the cloudbuild-delivery.yaml file available in the hello-cloudbuild-app repository and commit the change ####
 cd ~/hello-cloudbuild-env
 cp ~/hello-cloudbuild-app/cloudbuild-delivery.yaml ~/hello-cloudbuild-env/cloudbuild.yaml
-# cp ~/hello-cloudbuild-app/kubernetes.yaml ~/hello-cloudbuild-env
+
 git add .
 git commit -m "Create cloudbuild.yaml for deployment and cp kubernetes yaml"
 
 #### CREATE A CANDIDATE BRANCH AND PUSH TO UPDATE THE PRODUCTION AND CANDIDATE BRANCH ####
 git push origin production
+git checkout -b candidate
 git push origin candidate
 
 #### GRANT CLOUD BUILD THE SOURCE WRITER ROLE TO WRITE CODE TO THE HELLO-CLOUDBUIL-ENV CSR REPO ####
@@ -114,7 +116,7 @@ gcloud source repos set-iam-policy \
 #### CLOUD BUILD CD TRIGGER SHOULD CONTAIN A CLOUD BUILD YAML FILE THAT GRABS THE IMAGE FROM ARTIFACT REGISTRY AND DEPLOYS IT TO GKE, OR CLOUD DEPLOY ####
 gcloud beta builds triggers create cloud-source-repositories \
     --repo=hello-cloudbuild-env \
-    --branch-pattern=/^candidate$ \
+    --branch-pattern=^candidate$ \
     --build-config=/cloudbuild.yaml \
     --name=hello-cloudbuild-deploy
 
@@ -127,10 +129,6 @@ cd ~/hello-cloudbuild-app
 git add cloudbuild.yaml
 git commit -m "Trigger CD pipeline"
 git push google master
-
-# git add .
-# git commit -m "Trigger CD pipeline2"
-# git push google master
 
 #### GRANT THE CLOUD DEPLOY SA THE JOBRUNNER ROLE ####
  gcloud projects add-iam-policy-binding ${PROJECT_NUMBER} \
